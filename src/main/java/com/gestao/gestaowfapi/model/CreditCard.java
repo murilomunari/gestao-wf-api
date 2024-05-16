08de8c613e24c58ab58271f495f342061911e4b0
Murilo <munarimurilo441@gmail.com>
2024-05-16 11:39:21 -0300

    classe model terminada, e projeto esta subindo

---
+++ src/main/java/com/gestao/gestaowfapi/model/CreditCard.java
@@ -0,0 +1,35 @@
+package com.gestao.gestaowfapi.model;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("creditCards")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class CreditCard {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    private String number;
+
+    private Long cvv;
+
+    private Long year;
+
+    private String documentNumber;
+
+    private Long installments;
+
+    @DBRef
+    private Customer customer;
+}
---
+++ src/main/java/com/gestao/gestaowfapi/model/Order.java
@@ -0,0 +1,35 @@
+package com.gestao.gestaowfapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Document("orders")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Order {
+
+    @Id
+    private String id;
+
+    private BigDecimal originalPrice;
+
+    private BigDecimal discount;
+
+    private LocalDateTime dtResgisterOrder;
+
+    @DBRef
+    private Customer customer;
+
+    @DBRef
+    private Product product;
+}
