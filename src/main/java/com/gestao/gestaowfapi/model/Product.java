ff56f00c6ba5ecd989957f1e45efd1bf5ce7a5c2
Murilo <munarimurilo441@gmail.com>
2024-05-16 11:20:10 -0300

    classe model criada

---
+++ src/main/java/com/gestao/gestaowfapi/model/Product.java
@@ -0,0 +1,37 @@
+package com.gestao.gestaowfapi.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Document("products")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    @Size(min = 6,max = 20)
+    private String acronym;
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    private BigDecimal currentPrice;
+
+    private LocalDateTime dtCreation = LocalDateTime.now();
+}
