1b7f5282ac621dc23e77ad8eebfa6367c26f433a
Murilo <munarimurilo441@gmail.com>
2024-05-16 10:29:13 -0300

    Initial commit

---
+++ src/main/java/com/gestao/gestaowfapi/GestaoWfApiApplication.java
@@ -0,0 +1,13 @@
+package com.gestao.gestaowfapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoWfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoWfApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestao/gestaowfapi/GestaoWfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao.gestaowfapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoWfApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
