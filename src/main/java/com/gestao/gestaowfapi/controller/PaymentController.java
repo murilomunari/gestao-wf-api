949eee221bcc6b1d766a332d32e5d2a2b0eb66f0
Murilo <munarimurilo441@gmail.com>
2024-05-20 20:13:02 -0300

    erros arrumados e subindo aplicação (apos quebrar a cebeça)

---
+++ src/main/java/com/gestao/gestaowfapi/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.gestao.gestaowfapi.controller;
+
+import com.gestao.gestaowfapi.dto.PaymentDTO;
+import com.gestao.gestaowfapi.model.Payment;
+import com.gestao.gestaowfapi.service.PaymentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/payment")
+@RequiredArgsConstructor
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/credit-card")
+    public ResponseEntity<Mono<Payment>> create(@Valid @RequestBody PaymentDTO paymentDTO){
+        return ResponseEntity.ok().body(paymentService.process(paymentDTO));
+    }
+
+}
